ce78f3809e68a2bf2b642c2e514affe757a60ac1
Ivan Trutnev <i@gmail.com>
2019-01-28 18:15:41 +0300

    initial

---
+++ src/components/Login.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <form>
+          <div>
+            <label>
+              Name:
+            </label>
+            <input type="text" name="name"/>
+          </div>
+          <div>
+            <label>
+              Password:
+              <input type="text" name="password"/>
+            </label>
+          </div>
+          <input type="submit" value="Submit"/>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Login
---
+++ src/components/Main.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+import Login from './Login'
+import Home from './Home';
+import News from './News'
+import Profile from './Profile'
+
+// The Main component renders one of the three provided
+// Routes (provided that one matches). Both the /roster
+// and /schedule routes will match any pathname that starts
+// with /roster or /schedule. The / route will only match
+// when the pathname is exactly the string "/"
+class Main extends React.Component {
+  render() {
+    return (
+      <main>
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route path='/login' component={Login}/>
+          <Route path='/news' component={News}/>
+          <Route path='/profile' component={Profile}/>
+        </Switch>
+      </main>
+    )
+  }
+}
+
+export default Main
