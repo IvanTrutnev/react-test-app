21ef530e36e2211f7d2ca6b4da85ba52928228de
Ivan Trutnev <trutnev_1996@mail.ru>
2019-01-31 12:26:07 +0300

    add set auth reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,16 @@
+import { SET_AUTH } from '../constans/Login';
+
 export const initialState = {
 	user: {
 		isAuth: false
 	}
 };
 
-export function rootReducer(state = initialState) {
-	return state;
+export function rootReducer(state = initialState, action) {
+	switch (action.type) {
+		case SET_AUTH:
+			return {...state, isAuth: action.payload };
+		default:
+			return state;
+	}
 }
